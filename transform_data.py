7408b9f7e4579c7211d3378ce343a349e7bb08b6
jiahao25 <tanjiahao@hotmail.com>
2020-04-16 15:03:27 +0800

    added data processing functions

--- transform_data.py
+++ transform_data.py
@@ -1,12 +1,39 @@
 import pandas as pd
+import numpy as np
 
 #load dataset
 def load_dataset(dataset):
 	df = pd.read_csv(dataset)
 	return df
 
+def transform_df(df):
+
+	# drop rows with no name
+	df.dropna(axis='index',how='all',subset=['name'])
+
+	# add first_name and last_name
+	df['first_name']= df['name'].str.split().str[0] 
+	df['last_name'] = df['name'].str.split().str[1]
+	df.drop(columns='name',inplace=True)
+
+	# remove leading zeros
+	df['price'] = df['price'].astype(str).str.lstrip('0')
+	df['price'] = pd.to_numeric(df['price'])
+
+	# add new field, above_100
+	df['above_100'] = df['price'].apply(lambda x: True if x>100 else False)
+
+	# reorder columns
+	df = df[['first_name','last_name','price','above_100']]
+
+	#print(df.info())
+	#print(df)
+
+	return df_new
+
 if __name__ == '__main__':
 	dataset = 'dataset.csv'
-	result = load_dataset(dataset)
-	print(result)
+	df = load_dataset(dataset)
+	df_new = transform_df(df)
+
 
